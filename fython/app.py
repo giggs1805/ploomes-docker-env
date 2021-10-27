38d3e0dda25d40bb385c74b28bd8d4fcaeb8c860
Giggs (Linux) <heygiggs@tutanota.com>
2021-10-27 11:36:17 -0300

    Added new Python requests, classes and methods

--- fython/app.py
+++ fython/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from random import randint
 import pandas as pd
 from flask_cors import CORS
@@ -8,22 +8,67 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 df = pd.read_csv('pokemon.csv')
 START = 1
-# END = df['#'].max() + 1
 END = df['#'].max() + 1
 
-@app.route("/api/set_generations")
-def set_generations(val):
-    pass
 
-@app.route("/api/set_max_number")
-def set_max_number(val):
-    pass
+class Session:
+    def __init__(self, name, defaults):
+        self.ct = name
+        self.cr = defaults
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.ct == other
+        else:
+            return self.ct == other.ct
+
+    def getTrainer(self):
+        return self.ct
+
+    def getRolls(self):
+        return self.cr
+
+    def minusRoll(self):
+        self.cr -= 1
+
+
+sessionList = []
+Globals = { "rolls": 10 , "gens": 6 }
+
+
+@app.route("/api/subtract", methods=["POST"])
+def subtractOne():
+    req = request.json["name"]
+    index = sessionList.index(req)
+    sessionList[index].minusRoll()
+    rollsLeft = str(sessionList[index].getRolls())
+    print(req, index, sessionList[index].getRolls())
+    # sessionList[index]
+    return rollsLeft, 200
+
+
+@app.route("/api/config", methods=["POST"])
+def setGlobals():
+    req = request.json["name"]
+    Globals['rolls'] = req
+    Globals['gens'] = 6
+
+
+@app.route("/api/trainer", methods=["POST"])
+def checkDupes():
+    trainer = request.json["name"]
+    # print(trainer["name"])
+    if(trainer not in sessionList):
+        print("appending")
+        sessionList.append(Session(trainer, Globals['rolls']))
+    return '', 200
+
 
 @app.route("/api/random_encounter")
 def random_encounter():
     pokemonId = randint(START, END)
     pokemon = df[df['#'] == pokemonId].iloc[0]
-    print(pokemon)
+    # print(pokemon)
     isShiny = randint(0, 100) > 90
     shinyString = 'shiny/' if isShiny else ''
     pokemon['Shiny'] = isShiny
